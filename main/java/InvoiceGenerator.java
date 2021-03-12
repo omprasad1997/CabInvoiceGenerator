9fe3c1b8c6acd89ca711faf19d13c419f4f46530
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-12 13:10:01 +0530

    Welcome message

---
+++ main/java/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class InvoiceGenerator {
+    public void welcome(){
+        System.out.println("Welcome");
+    }
+}
---
+++ test/java/InvoiceServiceTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class InvoiceServiceTest {
+    public void welcome(){
+        System.out.println("Welcome");
+    }
+}
