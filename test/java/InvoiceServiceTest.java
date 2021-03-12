29e90a863a84275f6420c635aa276b082884694b
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-12 13:17:51 +0530

    Tested : Wrote Junit test to check total fare for multiple rides

--- test/java/InvoiceServiceTest.java
+++ test/java/InvoiceServiceTest.java
@@ -13,4 +13,16 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(25,fare,0.0);
     }
+
+    @Test
+    public void givenDistanceAndTime_ForMultipleRides_ShouldReturnTotalFare() {
+
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distanceFor1 = 3.0;
+        int timeFor1 = 6;
+        double distanceFor2 = 8.0;
+        int timeFor2 = 10;
+        double totalFare = invoiceGenerator.calculateFareForMultipleRides(distanceFor1, timeFor1,distanceFor2,timeFor2);
+        Assertions.assertEquals(126.0,totalFare,0.0);
+    }
 }
