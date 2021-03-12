c70776f090cc6f42939e356cb72937b0404a000d
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-12 13:13:25 +0530

    Updated: Calculate total fare for journey

--- main/java/InvoiceGenerator.java
+++ main/java/InvoiceGenerator.java
@@ -1,7 +1,10 @@
 import java.util.*;
 
 public class InvoiceGenerator {
-    public void welcome(){
-        System.out.println("Welcome");
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+
+    public double calculateFare(double distance, int time) {
+        return distance*MINIMUM_COST_PER_KILOMETER + time*COST_PER_TIME;
     }
 }
