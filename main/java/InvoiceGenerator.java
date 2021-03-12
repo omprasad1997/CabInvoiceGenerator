740d3cb5b7030d3745d3af4094b4d4c4922f4396
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-12 13:20:45 +0530

    Updated : Calculate total fare,total number of rides,average fare per ride for multiple rides

--- main/java/InvoiceGenerator.java
+++ main/java/InvoiceGenerator.java
@@ -11,4 +11,11 @@ public class InvoiceGenerator {
     public double calculateFareForMultipleRides(double distanceFor1, int timeFor1,double distanceFor2,int timeFor2) {
         return (distanceFor1+distanceFor2)*MINIMUM_COST_PER_KILOMETER + (timeFor1+timeFor2)*COST_PER_TIME;
     }
+
+    public String enhancedInvoice(int totalNumOfRides, double distanceFor1, int timeFor1, double distanceFor2, int timeFor2) {
+        double totalFare = (distanceFor1+distanceFor2)*MINIMUM_COST_PER_KILOMETER + (timeFor1+timeFor2)*COST_PER_TIME;
+        double averageFarePerRide = totalFare/totalNumOfRides;
+        return "Total number Of Rides: " + totalNumOfRides +"\n"+ "Total Fare: "+totalFare+"\n"
+                + "Average Fare Per Ride: " + averageFarePerRide;
+    }
 }
